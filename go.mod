2946ff54943f3034f8319d2ce5b3c798fa203391
Mateus Cazuza <mateus.cazuza@hotmail.com>
2023-08-21 06:36:08 -0300

    feat: folder structure

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module modulo
+module importModules
 
 go 1.21.0
 
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"modulo/auxiliar"
+	"importModules/auxiliar"
 
 	"github.com/badoux/checkmail"
 )
